89cc252515367ad94ba0a3025552ebe25ad63ccd
Usairam-s <muhammaduseram@gmail.com>
2023-12-16 21:25:17 +0500

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
 
-const app = express()
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("Connected to Database"))
+  .catch(() => console.log("Failed to connect to Datbase"));
 
-app.listen(3000, ()=>{
-    console.log("Server is running at 3000");
-})
+app.listen(3000, () => {
+  console.log("Server is running at 3000");
+});
