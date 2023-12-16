3e07eac35a604cf66761296929c034adad66b331
Usairam-s <muhammaduseram@gmail.com>
2023-12-16 20:59:22 +0500

    till header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
+import {BrowserRouter, Route , Routes} from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Signup from "./pages/Signup"
+import Signin from "./pages/Signin"
+import Profile from "./pages/Profile"
+import Header from "./components/Header"
+
 export default function App() {
-  return (
-    <h1 className="text-red-500 text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <BrowserRouter>
+  {/* Header */}
+  <Header/>
+  <Routes>
+    <Route path="/" element={<Home/>} />
+    <Route path="/about" element={<About/>} />
+    <Route path="/signup" element={<Signup/>} />
+    <Route path="/signin" element={<Signin/>} />
+    <Route path="/profile" element={<Profile/>} />
+  </Routes>
+  </BrowserRouter>
+  
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div className='bg-slate-200'>
+        <div className='flex mx-auto flex-row justify-between max-w-6xl p-3 items-center'>
+            <Link to="/">
+           <h2 className='font-semibold'>MERN Auth App</h2> 
+            </Link>
+
+            <ul className='flex gap-4'>
+                <Link to='/'><li>Home</li></Link>
+                <Link to='/about'><li>About</li></Link>
+                <Link to='/signin'><li>Signin</li></Link>
+            </ul>
+
+        </div>
+        
+    </div>
+  )
+}
+
+export default Header
